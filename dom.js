bb0b7659ee3ea0d875d097d22ff625a1b20d1038
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-02 21:39:23 +0530

    Task 3 (Dom manipulations)

---
+++ dom.js
@@ -0,0 +1,16 @@
+Title1 = document.querySelector('title');
+// console.log(Title1);
+
+Title1.innerText = 'New Item Listener';
+
+headerT = document.getElementById('header-title');
+header = document.getElementById('main-header')
+
+// console.log(headerT)
+
+headerT.textContent = 'Hello';
+header.style.borderBottom = 'solid 3px black'
+
+addItem = document.getElementById('title');
+
+addItem.style.color = 'green'
