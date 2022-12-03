b0a57eab4ba4cc2518c74938138109c8200329ec
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-03 12:14:00 +0530

    Task6 querySelector Added

--- Task4.js
+++ Task4.js
@@ -11,7 +11,14 @@
 
 
 // Task 5
-li = document.getElementsByTagName('li');
-for(var i=0;i<li.length;i++){
-    li[i].style.backgroundColor = 'gray';
-}
+// li = document.getElementsByTagName('li');
+// for(var i=0;i<li.length;i++){
+//     li[i].style.backgroundColor = 'gray';
+// }
+
+// Task 6 querySelector
+li = document.querySelector('li:nth-child(2)');
+li.style.backgroundColor = 'green';
+
+li = document.querySelector('li:nth-child(3)');
+li.style.display = 'none';
