f41b7c2c6a52f5fd6befa868fce381c1b90f250a
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-03 12:20:32 +0530

    Task 6 querySelectorAll added

--- Task4.js
+++ Task4.js
@@ -17,8 +17,18 @@
 // }
 
 // Task 6 querySelector
-li = document.querySelector('li:nth-child(2)');
-li.style.backgroundColor = 'green';
+// li = document.querySelector('li:nth-child(2)');
+// li.style.backgroundColor = 'green';
 
-li = document.querySelector('li:nth-child(3)');
-li.style.display = 'none';
+// li = document.querySelector('li:nth-child(3)');
+// li.style.display = 'none';
+
+// QuerySelectorALL
+li = document.querySelectorAll('li');
+li[1].style.color = 'green';
+
+odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+}
