da67fe6d6cb0b616c59a2d1955b236f23e6c2bb6
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2022-12-03 15:08:37 +0530

    Modification for Task 9 Filter project Added

--- Task8.js
+++ Task8.js
@@ -1,16 +1,23 @@
 form = document.getElementById('addForm');
 itemList = document.getElementById('items');
+filter = document.getElementById('filter');
 
 form.addEventListener('submit',addItem);
 itemList.addEventListener('click',removeItem);
+filter.addEventListener('keyup',filterElements);
 
 function addItem(e){
     e.preventDefault();
     var t = document.getElementById('item').value;
+    var d = document.getElementById('description').value;
+
     var li = document.createElement('li');
     li.className = 'list-group-item';
 
+    var des = document.createElement('div');
+
     li.appendChild(document.createTextNode(t));
+    des.appendChild(document.createTextNode(' '+d));
 
     editButton = document.createElement('button');
     editButton.className = 'btn btn-success btn-sm float-right edit';
@@ -23,7 +30,7 @@ function addItem(e){
 
     li.appendChild(delButton);
     li.appendChild(editButton);
-
+    li.appendChild(des);
     itemList.appendChild(li);
 }
 
@@ -32,4 +39,22 @@ function removeItem(e){
         var li = e.target.parentElement;
         itemList.removeChild(li);
     }
+}
+
+
+function filterElements(e){
+    var text = e.target.value.toLowerCase();
+
+    liList = itemList.getElementsByTagName('li');
+
+    Array.from(liList).forEach(function(li){
+        var name = li.textContent;
+
+        if(name.toLowerCase().indexOf(text) != -1){
+            li.style.display = 'block';
+        }
+        else{
+            li.style.display = 'none';
+        }
+    })
 }
